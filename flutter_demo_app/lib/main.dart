c4d5c0900c2a7ed784a6b4b7fe66ad8d47931c0a
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-09-02 18:40:58 +0200

    Refactor existing code into separate page
    - Change code which checks for window width

--- flutter_demo_app/lib/main.dart
+++ flutter_demo_app/lib/main.dart
@@ -1,34 +1,12 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_app/pages/hello_world.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: Text(
-            'Hello world',
-          ),
-        ),
-        body: Center(
-          child: Container(
-            color: Colors.blue,
-            width: window.physicalSize.width * 0.90,
-            child: Text(
-              'Hello world...',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 64,
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: HelloWorld(),
     ),
   );
 }
